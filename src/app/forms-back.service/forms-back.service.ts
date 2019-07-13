748fe76b2bc8f82b7c99c0e75b253a27b8cf325c
Jorge <jorgecampoy81@gmail.com>
2019-07-13 08:16:53 +0200

    añadidos algunos colores

--- src/app/forms-back.service/forms-back.service.ts
+++ src/app/forms-back.service/forms-back.service.ts
@@ -9,63 +9,67 @@ export class FormsBackService {
     {
       id: 1,
       idx: 0,
-      nombre: 'Tomate'
+      nombre: 'Tomate',
+      color: 'cyan'
     },
     {
       id: 2,
       idx: 1,
-      nombre: 'Patata'
+      nombre: 'Patata',
+      color: 'pink'
     },
     {
       id: 3,
       idx: 2,
-      nombre: 'Zanahoria'
+      nombre: 'Zanahoria',
+      color: 'Salmon'
     },
     {
       id: 4,
       idx: 3,
-      nombre: 'Melón'
+      nombre: 'Melón',
+      color: 'lightGreen'
     },
-    {
-      id: 5,
-      idx: 4,
-      nombre: 'Sandía'
-    },
-    {
-      id: 6,
-      idx: 5,
-      nombre: 'Guisante'
-    },
-    {
-      id: 7,
-      idx: 6,
-      nombre: 'Pimiento'
-    },
-    {
-      id: 8,
-      idx: 7,
-      nombre: 'Kiwi'
-    },
-    {
-      id: 9,
-      idx: 8,
-      nombre: 'Platano'
-    },
-    {
-      id: 10,
-      idx: 9,
-      nombre: 'Pera'
-    },
-    {
-      id: 11,
-      idx: 10,
-      nombre: 'Manzana'
-    },
-    {
-      id: 12,
-      idx: 11,
-      nombre: 'Paraguaya'
-    },
+    // {
+    //   id: 5,
+    //   idx: 4,
+    //   nombre: 'Sandía'
+    // },
+    // {
+    //   id: 6,
+    //   idx: 5,
+    //   nombre: 'Guisante'
+    // },
+    // {
+    //   id: 7,
+    //   idx: 6,
+    //   nombre: 'Pimiento'
+    // },
+    // {
+    //   id: 8,
+    //   idx: 7,
+    //   nombre: 'Kiwi'
+    // },
+    // {
+    //   id: 9,
+    //   idx: 8,
+    //   nombre: 'Platano'
+    // },
+    // {
+    //   id: 10,
+    //   idx: 9,
+    //   nombre: 'Pera'
+    // },
+    // {
+    //   id: 11,
+    //   idx: 10,
+    //   nombre: 'Manzana'
+    // },
+    // {
+    //   id: 12,
+    //   idx: 11,
+    //   nombre: 'Paraguaya'
+    // },
   ];
 
   constructor() {
